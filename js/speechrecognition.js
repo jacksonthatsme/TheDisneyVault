5ba30440034d4123eecffaaf3a702d9ff08034c2
Jackson Lynch <jacksonthatsme@gmail.com>
2013-09-15 17:58:04 -0400

    added initial speech recognizer + cname

---
+++ js/speechrecognition.js
@@ -0,0 +1,38 @@
+if (!('webkitSpeechRecognition' in window)) {
+        alert('Web speech API is not supported in this browser');
+      } 
+    else {
+
+        // Speech recognition init
+        var recognition = new webkitSpeechRecognition();
+
+        // continously listen to speech
+        recognition.continuous = true;
+
+        // set languages supported
+        recognition.lang = ['English', ['en-US', 'United States']];
+
+        // We return non-final strings so gameplay isn't laggy
+        recognition.interimResults = true;
+
+        recognition.onresult = function(e) {
+
+        // set variable 
+        var interim_transcript = '';
+          if (e.results.length) {
+             for (var i = event.resultIndex; i < event.results.length; i++) {
+                 interim_transcript = event.results[i][0].transcript; 
+
+                 // if the object isn't moving, allow commands to be sent down.
+                 if(!move){gameLoop(interim_transcript,'voice');}
+              }
+            
+           
+          }
+        };
+      // start speech to text translation
+      recognition.start();
+
+   }
+   
+   recognition.onsoundstart = console.log('sound has started');
