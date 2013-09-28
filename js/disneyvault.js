e1f330e10ed5bf504508fdaf95d7c34944a1ba30
Jackson Lynch <jacksonthatsme@gmail.com>
2013-09-28 17:17:34 -0400

    added video alpha to index

--- js/disneyvault.js
+++ js/disneyvault.js
@@ -1,16 +1,13 @@
+
 $( document ).ready(function() {
     $( ".loading" ).delay(3000).fadeOut();
-    $('#microphone-modal').modal();
+    //$('#microphone-modal').modal();
+    $('#alpha_dust').seeThru({
+			width: '100%',
+			height: '100%'
+	});
+    
 });
-
-//RUMBLE ON MODAL
-$('#microphone-modal').jrumble({
-		x: .5,
-		y: 2,
-		rotation: 0,
-		speed: 160
-});
-$('#microphone-modal').trigger('startRumble');
 
 //PAGE DOWN MARGIN
 var wh = $(window).height();
