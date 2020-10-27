755e96fa223652d89f25fb4da74cefd5696d42cd
Jackson <jacksonthatsme@gmail.com>
2020-10-26 23:58:27 -0400

    fixed some issues

--- js/disneyvault.js
+++ js/disneyvault.js
@@ -10,59 +10,65 @@ $( document ).ready(function() {
 			start:'external',
 			end: 'stop'
 	});
-    
-});
 
-//PAGE DOWN MARGIN
-var wh = $(window).height();
+  //PAGE DOWN MARGIN
+  var wh = $(window).height();
 
-$('.pagedown').css('margin-bottom', (wh/3) );
+  $('.pagedown').css('margin-bottom', (wh/3) );
 
-$('.simplemodal-close').click(function(){
-	$('body').removeClass('pre-dust');
-	$('canvas').hide();
-});
+  $('.simplemodal-close').click(function(){
+  	$('body').removeClass('pre-dust');
+  	$('canvas').hide();
+  });
 
-//MODAL LINKS
-$('.about-link').click(function(){
-	$('#about').modal({opacity:80, overlayClose: true});
-});
-$('.legal-link').click(function(){
-	$('#legal').modal({opacity:80, overlayClose: true});
-});
-$('.colophon-link').click(function(){
-	$('#colophon').modal({opacity:80, overlayClose: true});
-});
+  //MODAL LINKS
+  $('.about-link').click(function(){
+  	$('#about').modal({opacity:80, overlayClose: true});
+  });
+  $('.legal-link').click(function(){
+  	$('#legal').modal({opacity:80, overlayClose: true});
+  });
+  $('.colophon-link').click(function(){
+  	$('#colophon').modal({opacity:80, overlayClose: true});
+  });
 
-$('area').click(function(){
-	var dust = document.getElementById('alpha_dust');
+  $('area').click(function(){
+  	var dust = document.getElementById('alpha_dust');
 
-    $('#microphone-modal h3').text("Allow use of your microphone above.");
-    $('img.microphone-button').removeClass('mic-blink');
-    $('img.microphone-button').addClass('mic-on');
-    $('#microphone-modal h5').remove();
-    whistle.init(null,null,'low');
+      $('#microphone-modal h3').text("Allow use of your microphone above.");
+      $('img.microphone-button').removeClass('mic-blink');
+      $('img.microphone-button').addClass('mic-on');
+      $('#microphone-modal h5').remove();
+      whistle.init(null,null,'low');
 
-});
-document.addEventListener("whistleReady", function() {
-    $('#microphone-modal h3').text("Now blow...");
-}, false);
-document.addEventListener("whistle", function() { 
-    if (whistle.intensity > 100) {
-      console.log("Whistle intensity is above 100");
-      var dust = document.getElementById('alpha_dust');
-      dust.play();
-      $.modal.close();
-      $('body').removeClass('pre-dust');
-      
-      document.removeEventListener('whistle');
+  });
+  document.addEventListener("whistleReady", function() {
+      $('#microphone-modal h3').text("Now blow...");
+
+      setTimeout(function(){
+        var dust = document.getElementById('alpha_dust');
+        dust.play();
+        $.modal.close();
+        $('body').removeClass('pre-dust');
+      }, 2000);
+  }, false);
+  // document.addEventListener("whistle", function() {
+  //     if (whistle.intensity > 100) {
+  //       console.log("Whistle intensity is above 100");
+  //       var dust = document.getElementById('alpha_dust');
+  //       dust.play();
+  //       $.modal.close();
+  //       $('body').removeClass('pre-dust');
+  //
+  //       document.removeEventListener('whistle');
+  //     }
+  // }, false);
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 960){
+      $('.dvd').addClass('animate');
+
     }
-}, false);
+  });
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 960){
-    $('.dvd').addClass('animate');
-
-  }
 });
-
